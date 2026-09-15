fe0c04589ae1baee262ffa216ca211e6bfee3e0f
NormO <normo.pub@gmail.com>
2026-09-14 19:06:05 -0700

    uni: islands register their unmounters

--- fsr/examples/uni/js/src/main.ts
+++ fsr/examples/uni/js/src/main.ts
@@ -1,37 +1,42 @@
 import htmx from "htmx.org";
 import { boot, enableNavigation, live, registerIsland } from "@snapfire/fsr-client";
 import { bindHtmx } from "@snapfire/fsr-client/htmx";
-import { reactMounter, reactPatcher } from "@snapfire/fsr-client/react";
-import { vueMounter, vuePatcher } from "@snapfire/fsr-client/vue";
+import { reactMounter, reactPatcher, reactUnmounter } from "@snapfire/fsr-client/react";
+import { vueMounter, vuePatcher, vueUnmounter } from "@snapfire/fsr-client/vue";
 
 registerIsland("js/src/ui/Watch.tsx#default", {
   loader: () => import("./ui/Watch.js").then((m) => m.default),
   mount: reactMounter,
   patch: reactPatcher,
+  unmount: reactUnmounter,
 });
 
 registerIsland("js/src/ui/Feed.tsx#default", {
   loader: () => import("./ui/Feed.js").then((m) => m.default),
   mount: reactMounter,
   patch: reactPatcher,
+  unmount: reactUnmounter,
 });
 
 registerIsland("js/src/ui/Chip.tsx#default", {
   loader: () => import("./ui/Chip.js").then((m) => m.default),
   mount: reactMounter,
   patch: reactPatcher,
+  unmount: reactUnmounter,
 });
 
 registerIsland("js/src/ui/Sparkline.vue#default", {
   loader: () => import("./ui/Sparkline.vue").then((m) => m.default),
   mount: vueMounter,
   patch: vuePatcher,
+  unmount: vueUnmounter,
 });
 
 registerIsland("js/src/ui/Holdings.vue#default", {
   loader: () => import("./ui/Holdings.vue").then((m) => m.default),
   mount: vueMounter,
   patch: vuePatcher,
+  unmount: vueUnmounter,
 });
 
 boot();
